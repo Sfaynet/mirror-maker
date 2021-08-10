1c5bebfad4aee49bdf6fcf037468f7572dc189eb
Stanislav Somsikov <ssomsikov@ivi.ru>
2021-08-10 12:21:02 +0300

    123

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ set -e
 
 envsubst < /opt/mirrormaker/mm2.template > /var/run/mirrormaker/mm2.config
 
-/opt/bitnami/kafka/bin/connect-mirror-maker.sh /var/run/mirrormaker/mm2.config $MM2_S_ENVS
+/opt/bitnami/kafka/bin/connect-mirror-maker.sh /var/run/mirrormaker/mm2.config
